ca6da1ec7a10cde8b364626269a62a2571570977
Giovanna Resende <giovannaresende0475@gmail.com>
2023-12-12 18:58:52 -0300

    feature/CF002: Alteração do main.py com o core da comunicação no telegram. Inserido métodos de receber mensagem, interpretar, e atualização para a versão mais recente do python-telegram-bot

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
-from binance import Client
-from telegram import Bot
+import re
+import logging
+from binance.client import Client
 from decouple import config
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, CallbackContext
 
-# Chaves da API da Binance
-api_key = config('BINANCE_API_KEY')
-api_secret = config('BINANCE_API_SECRET')
 
-# Token do bot do Telegram
-telegram_token = config('TELEGRAM_TOKEN')
+# Configurações
+BINANCE_API_KEY = config('BINANCE_API_KEY')
+BINANCE_API_SECRET = config('BINANCE_API_SECRET')
+TELEGRAM_TOKEN = config('TELEGRAM_TOKEN')
 
-# ID do grupo no Telegram
-group_id = int(config('TELEGRAM_GROUP_ID'))
+# Clientes API
+binance_client = Client(api_key=BINANCE_API_KEY, api_secret=BINANCE_API_SECRET)
 
-# Inicializar clientes da Binance e do Telegram
-binance_client = Client(api_key, api_secret)
-telegram_bot = Bot(token=telegram_token)
+# Configuração de Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+
+class TradingBot:
+    def __init__(self):
+        self.pattern = re.compile(r'\$(\w+) / (\w+)\s*COMPRA \((\d+x)\)\s*([\d.]+ a [\d.]+)\s*Take profit:\s*(.*?)Stop loss:\s*([\d.]+)', re.DOTALL)
+
+    async def interpretar_mensagem(self, mensagem):
+        logger.info(f"Recebendo mensagem para interpretar: \n{mensagem}")
+        match = self.pattern.match(mensagem)
+        if match:
+            par_trading_moeda, par_trading_contra, alavancagem, intervalo_precos, alvos, stop_loss = match.groups()
+            par_trading = f"{par_trading_moeda} / {par_trading_contra}"
+            logger.info(f"Mensagem interpretada: \nPar de Trading: {par_trading}, Alavancagem: {alavancagem}, Intervalo de Preços: {intervalo_precos}, Alvos: {alvos}, Stop Loss: {stop_loss}")
+            # Processamento adicional aqui
+        else:
+            logger.warning("A mensagem não corresponde ao padrão esperado.")
+
+
+async def receber_mensagens(update: Update, context: CallbackContext):
+    logger.info(f"Mensagem recebida: {update.message.text}")
+    bot = context.bot_data.setdefault("bot", TradingBot())
+    await bot.interpretar_mensagem(update.message.text)
+
+
+def main():
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, receber_mensagens)
+    application.add_handler(message_handler)
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
